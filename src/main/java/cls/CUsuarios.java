e0a6f442e6f89fb4ff509e79722cfcc2686898b9
arima <arima@Marijo_Magaa>
2025-03-27 08:58:27 -0600

    COMMIT

--- src/main/java/cls/CUsuarios.java
+++ src/main/java/cls/CUsuarios.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package cls;
 
 import java.sql.PreparedStatement;
@@ -13,10 +9,6 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author arima
- */
 public class CUsuarios {
     
         public void mostrarUsuarios(JTable tablaUsuarios) {
